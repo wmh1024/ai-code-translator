e9f0eb6cc9360b8a2c2a023f9e5a657ebb270327
wmh <wmh031024@163.com>
2024-05-06 15:30:40 +0800

    vercel commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,8 +18,9 @@ export default function Home() {
   const [hasTranslated, setHasTranslated] = useState<boolean>(false);
   const [apiKey, setApiKey] = useState<string>('');
   const [baseUrl, setBaseUrl] = useState<string>('https://ai.wmhwiki.cn/api');
-  // const [baseUrl, setBaseUrl] = useState<string>('');
 
+  // const [baseUrl, setBaseUrl] = useState<string>('');
+  
   const handleTranslate = async () => {
     const maxCodeLength = model === 'gpt-3.5-turbo' ? 6000 : 12000;
 
